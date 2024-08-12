d4f5b3df1fd71fc84e6bfd5fda2f8b0b9be8050d
SashaPotemkin <alex.potjomkin@gmail.com>
2024-08-12 14:29:33 +0300

    Deprecated метод

---
+++ app/src/main/java/ru/potemkin/activityresultapikotlin/MainActivity.kt
@@ -0,0 +1,60 @@
+package ru.potemkin.activityresultapikotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var getUsernameButton: Button
+    private lateinit var usernameTextView: TextView
+    private lateinit var getImageButton: Button
+    private lateinit var imageFromGalleryImageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initViews()
+        getUsernameButton.setOnClickListener {
+            UsernameActivity.newIntent(this).apply {
+                startActivityForResult(this, RC_USERNAME)
+            }
+        }
+        getImageButton.setOnClickListener {
+            Intent(Intent.ACTION_PICK).apply {
+                type = "image/*"//MIME TYPES
+                startActivityForResult(this, RC_IMAGE)
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode== RC_USERNAME && resultCode== RESULT_OK){
+            val username = data?.getStringExtra(UsernameActivity.EXTRA_USERNAME) ?:""
+            usernameTextView.text = username
+        }
+        if(requestCode== RC_IMAGE && resultCode== RESULT_OK){
+            val uri = data?.data
+            imageFromGalleryImageView.setImageURI(uri)
+        }
+    }
+
+    private fun initViews() {
+        getUsernameButton = findViewById(R.id.get_username_button)
+        usernameTextView = findViewById(R.id.username_textview)
+        getImageButton = findViewById(R.id.get_image_button)
+        imageFromGalleryImageView = findViewById(R.id.image_from_gallery_imageview)
+    }
+
+
+    companion object{
+        private const val RC_USERNAME = 100
+        private const val RC_IMAGE = 101
+    }
+}
+
---
+++ app/src/main/java/ru/potemkin/activityresultapikotlin/UsernameActivity.kt
@@ -0,0 +1,40 @@
+package ru.potemkin.activityresultapikotlin
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+class UsernameActivity : AppCompatActivity() {
+
+    private lateinit var usernameEditText: EditText
+    private lateinit var saveUsernameButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_username)
+        initViews()
+        saveUsernameButton.setOnClickListener {
+            val username = usernameEditText.text.trim().toString()
+            saveUsername(username)
+            finish()
+        }
+    }
+
+    private fun initViews() {
+        usernameEditText = findViewById(R.id.username_edittext)
+        saveUsernameButton = findViewById(R.id.save_username_button)
+    }
+
+    private fun saveUsername(username: String) {
+        Intent().apply {
+            putExtra(EXTRA_USERNAME,username)
+            setResult(RESULT_OK,this)
+        }
+    }
+
+    companion object {
+        const val EXTRA_USERNAME="username"
+        fun newIntent(context: Context) = Intent(context, UsernameActivity::class.java)
+    }
+}
